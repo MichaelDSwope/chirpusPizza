6e178286a771495cb3dbede8d468e3fbf269e7f7
MichaelDSwopeVox <digitalwhiterabbithole@gmail.com>
2023-11-07 18:58:52 -0500

    first commit before adding bonus css or bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menu';
+
+  headers = ["ID", "Name", "Category", "Price"];
+
+  rows = [
+    {
+      "ID": "1",
+      "Name": "Chicken Fingers",
+      "Category": "Dinner",
+      "Price": "11.99"
+    },
+    {
+      "ID": "2",
+      "Name": "Pizza",
+      "Category": "Dinner",
+      "Price": "11.99"
+    },
+    {
+      "ID": "3",
+      "Name": "Wings",
+      "Category": "Sides",
+      "Price": "8.99"
+    },
+    {
+      "ID": "4",
+      "Name": "Breadsticks",
+      "Category": "Sides",
+      "Price": "4.99"
+    },
+    {
+      "ID": "5",
+      "Name": "Caesar Salad",
+      "Category": "Salads",
+      "Price": "5.99"
+    },
+    {
+      "ID": "6",
+      "Name": "Cinnamon Rolls",
+      "Category": "Dessert",
+      "Price": "8.99"
+    },
+  ]
 }
